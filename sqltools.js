f17cddfce72c65e7175be6d4444f3cfe2b2338b6
Arun Ramakani <arun@hubun.ae>
2019-09-21 22:22:14 +0400

    message

--- sqltools.js
+++ sqltools.js
@@ -1,32 +1,36 @@
-// Load My SQL node Module
-var mysql = require('mysql');
+module.exports = {
 
-// mysql host and connection module variable
-var host = "mysql"
-var con
+    // Load My SQL node Module
+    var mysql = require('mysql');
 
-//load host name to the node module and load connection
-function loadSQLHost(dns) {
-    host = dns
+    // mysql host and connection module variable
+    var host = "mysql"
+    var con
 
-    con = mysql.createConnection({
-        host: host,
-        user: "root",
-        password: "password",
-        database: "user"
-    });
-}
+    //load host name to the node module and load connection
+    function loadSQLHost(dns) {
+        host = dns
+
+        con = mysql.createConnection({
+            host: host,
+            user: "root",
+            password: "password",
+            database: "user"
+        });
+    }
 
 
-// receives  data and log them into My SQL
-function pushUserDataToDB(users) {
+    // receives  data and log them into My SQL
+    function pushUserDataToDB(users) {
 
-    //SQL statement and place holder for batch insert
-    var sql = "INSERT INTO user.usr (id, name, message) VALUES ?";
+        //SQL statement and place holder for batch insert
+        var sql = "INSERT INTO user.usr (id, name, message) VALUES ?";
 
-    con.query(sql, [users], function (err) {
-        if (err) throw err;
-        con.end();
-    });
+        con.query(sql, [users], function (err) {
+            if (err) throw err;
+            con.end();
+        });
 
-}
+    }
+
+};
